96868eb3298ee409bb85190bbf28045b087b0500
Michael Berg <mberg12@bu.edu>
2023-07-11 13:46:43 -0400

    first commit

---
+++ Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BestBUY_Dapper
+{
+	public class Department
+	{
+		public Department()
+		{
+		}
+
+		public int DepartmentID { get; set; }
+		public string Name { get; set; }
+	}
+}
+
---
+++ IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BestBUY_Dapper
+{
+	public interface IDepartmentRepository
+	{
+		public IEnumerable<Department> GetAllDepartments();
+		public void InsertDepartment(string newDepartmentName);
+
+	}
+
+}
+
