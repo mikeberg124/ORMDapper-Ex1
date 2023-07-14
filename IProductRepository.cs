c536320a1dc5f4c79b4fac70f30e95d1fa28de74
Michael Berg <mberg12@bu.edu>
2023-07-14 16:15:46 -0400

    exercise 2

---
+++ IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BestBUY_Dapper
+{
+	public interface IProductRepository
+	{
+		public IEnumerable<Product> GetAllProducts();
+		public void CreateProduct(string name, double price, int categoryID);
+		public void UpdateProduct(int productID, string updatedName);
+		public void DeleteProduct(int productID);
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using BestBUY_Dapper;
+using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System.Data;
 
@@ -11,3 +12,38 @@ string connString = config.GetConnectionString("DefaultConnection");
 
 IDbConnection conn = new MySqlConnection(connString);
 
+
+var repo = new DapperProductRepository(conn);
+
+Console.WriteLine("What is the name of your new product?");
+var prodName = Console.ReadLine();
+
+Console.WriteLine("What is the price?");
+var prodPrice = double.Parse(Console.ReadLine());
+
+Console.WriteLine("What is the categoryID?");
+var prodCat = int.Parse(Console.ReadLine());
+
+repo.CreateProduct(prodName, prodPrice, prodCat);
+
+var prodList = repo.GetAllProducts();
+
+foreach (var prod in prodList)
+{
+    Console.WriteLine($"{prod.ProductID} - {prod.Name}");
+}
+
+
+Console.WriteLine("What is the ProductID you want to update?");
+var prodID = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What is the new procudt name?");
+var newName = Console.ReadLine();
+
+repo.UpdateProduct(prodID, newName);
+
+
+Console.WriteLine("What is the prodcut ID you want to delete?");
+prodID = int.Parse(Console.ReadLine());
+
+repo.DeleteProduct(prodID);
